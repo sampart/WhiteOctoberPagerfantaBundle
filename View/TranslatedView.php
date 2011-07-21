1380c05e9beb7ded983dbbf53b32abb89d70c8fa
Jérôme Tamarelle <jerome@tamarelle.net>
2011-07-21 23:17:13 +0200

    Create TranslatedView to render DefaultView with translated texts.

---
+++ View/TranslatedView.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+ * This file is part of the Pagerfanta package.
+ *
+ * (c) Pablo Díez <pablodip@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WhiteOctober\PagerfantaBundle\View;
+
+use Pagerfanta\PagerfantaInterface;
+use Pagerfanta\View\ViewInterface;
+use Symfony\Component\Translation\TranslatorInterface;
+
+/**
+ * Translated view.
+ * 
+ * This view renders the default view with texts translated to the user language.
+ * 
+ * @author Jérôme Tamarelle <jerome@tamarelle.net>
+ */
+class TranslatedView implements ViewInterface
+{
+    /**
+     * @var \Pagerfanta\View\ViewInterface
+     */
+    private $view;
+
+    /**
+     * @var \Symfony\Component\Translation\TranslatorInterface
+     */
+    private $translator;
+
+    /**
+     * Constructor.
+     *
+     * @param ViewInterface       $view
+     * @param TranslatorInterface $translator
+     */
+    public function __construct(ViewInterface $view, TranslatorInterface $translator)
+    {
+        $this->view = $view;
+        $this->translator = $translator;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function render(PagerfantaInterface $pagerfanta, $routeGenerator, array $options = array())
+    {
+        if (!isset($options['previous_message'])) {
+            $options['previous_message'] = $this->translator->trans('Previous');
+        }
+
+        if (!isset($options['next_message'])) {
+            $options['next_message'] = $this->translator->trans('Next');
+        }
+
+        return $this->view->render($pagerfanta, $routeGenerator, $options);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'translated';
+    }
+}
