586a3331b47658b3322fd2aaa72d884038f775cc
Wenming Tang <wenmingtang@users.noreply.github.com>
2018-01-03 09:28:23 +0800

    Create TwitterBootstrap4 Translated View

---
+++ Tests/View/TwitterBootstrap4TranslatedViewTest.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * This file is part of the Pagerfanta package.
+ *
+ * (c) Pablo Díez <pablodip@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Wenming Tang <wenming@cshome.com>
+ */
+namespace WhiteOctober\PagerfantaBundle\Tests\View;
+
+class TwitterBootstrap4TranslatedView extends TwitterBootstrapTranslatedViewTest
+{
+    protected function viewClass()
+    {
+        return 'Pagerfanta\View\TwitterBootstrap4View';
+    }
+    protected function translatedViewClass()
+    {
+        return 'WhiteOctober\PagerfantaBundle\View\TwitterBootstrap4TranslatedView';
+    }
+    protected function translatedViewName()
+    {
+        return 'twitter_bootstrap4_translated';
+    }
+}
